0a8083185030698f00bfbef89cc9dbb4bfc9b1fd
qbasa <qbasa@wp.pl>
2017-05-22 11:25:33 +0200

    countries

---
+++ scripts.js
@@ -0,0 +1,30 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+$("#search").click(function() {
+    searchCountries();
+});
+
+function searchCountries() {
+    var countryName = $('#country-name').val();
+    if (!countryName.length) countryName = 'Poland';
+    $.ajax({
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
+    });
+}
+
+function showCountriesList(resp) {
+    countriesList.empty();
+}
+
+function showCountriesList(resp) {
+    countriesList.empty();
+    resp.forEach(function(item) {
+        resp.forEach(function(item) {
+            $('<li style="color: #1b0aff;">').text("Country: " + item.name).appendTo(countriesList);
+            $('<li style="color: #a75656;">').text("Capital: " + item.capital).appendTo(countriesList);
+            $('<li style="color: #14a921;">').text("Region: " + item.region).appendTo(countriesList);
+        });
+    });
+}
